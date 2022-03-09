7e1366fe48623efd15b5bdd3c71980a397e6e3fd
Exp <biellbigama@gmail.com>
2022-03-09 09:14:49 -0300

    Teste finalizado

--- jest.config.js
+++ jest.config.js
@@ -137,7 +137,7 @@ module.exports = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    "<rootDir>/config/jest/setupTests.ts"
+    "<rootDir>/src/setupTest.ts"
   ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
